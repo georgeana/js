e09b2bccac5a4d6c02074c428aec0c055d17a3c2
Queen G <Queen G>
2017-03-21 11:09:57 +0100

    first javascript

---
+++ main.js
@@ -0,0 +1,15 @@
+let name = "georgeana";
+
+let numKids = 1;
+
+let address = "Brunevang 24";
+
+let city = "Copenhagen";
+
+let namePartner = "Stefan";
+
+let ocupation = "freelancer";
+
+let info = name + "will be a" + ocupation + "in" + city + "and married to" + namePartner + "with" numKids + "kid";
+console.log(info);
+
