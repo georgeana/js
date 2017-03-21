b342b5c318191e1722d20dd26c6fb5d7bfffce59
Queen G <Queen G>
2017-03-21 12:35:36 +0100

    first js exercise

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-let name = "georgeana";
+let anim = document.querySelector("#anim");
+anim.addEventListener("click" , startAnimation);
 
-let numKids = 1;
-
-let address = "Brunevang 24";
-
-let city = "Copenhagen";
-
-let namePartner = "Stefan";
-
-let ocupation = "freelancer";
-
-let info = name + "will be a" + ocupation + "in" + city + "and married to" + namePartner + "with" numKids + "kid";
-console.log(info);
-
+function startAnimation(){
+    anim.classList.add("enable");
+}
